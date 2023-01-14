620ea7d8a7c339a53ed8c363efbe8b6c977ef8c8
Pankaj <rcypankaj@gmail.com>
2023-01-15 01:57:54 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// buttons showModel-1, showModel-2,showModel-3
+const button = document.querySelectorAll(".button");
+
+const overlay = document.querySelector(".overlay");
+
+// pop up window
+const popped = document.querySelector(".popped");
+
+// function for open window
+const openModel = function () {
+  popped.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+// function for close window
+const closeModel = function () {
+  popped.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i of button) {
+  i.addEventListener("click", openModel);
+}
+
+const btnCloseModel = document.querySelector(".close-Model");
+
+btnCloseModel.addEventListener("click", closeModel);
